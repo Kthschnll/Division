a581a9f5c33f60c9d88547a420edca658ec19890
Kthschnll <kthschnll@t-online.de>
2019-01-08 11:42:56 +0100

    Verbessert

--- main.c
+++ main.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <math.h>
-
-int teile (int i, int j, int *e, int *r);
-int main() {
 int dd, ds, *zd, *zs;
+
+int teile ();
+int main() {
+
     printf("Gib einen Dividenten ein:\n");
     scanf ("%d", &dd);
     printf("Gib einen Divisor ein:\n");
     scanf("%d", &ds);
     zd = &dd;
     zs = &ds;
-    int quo;
-    quo = teile(dd, ds, zd, zs);
 
-    printf("Der Quotient ist : %d \n",quo);
+    teile(zd, zs);
+
+    printf("Der Quotient aus %d / %d ist: %d Rest %d\n",dd, ds, *zd, *zs);
     return 0;
 }
 
-int teile (int i, int j, int *zd, int *r) {
-int quo;
-int re ;
+int teile () {
+
+int quo, i, j, r;
+j = *zs;
+i = *zd;
 if (j == 0){return 0; }
-
-quo = i / j;
-
-zd = &quo;
-
+else {
+    quo = i / j;
+    r = i%j;
+    zd = &quo;
+    zs = &r;
+}
 return 1;
 }
